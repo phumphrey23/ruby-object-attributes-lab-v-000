85c76e6161587131586ac40fd1295ea5fbd479c4
Precious Humphrey <phumphrey23@gmail.com>
2018-03-17 00:01:28 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+def name=(Beyonce)
+    @name = Beyonce
+  end
+
+  def name
+    @name
+  end
+
+  def job=(singer)
+      @job = singer
+    end
+
+    def job
+      @job
+    end
+end
